720b1f21162f2e11b409e72f10d8c5bb6bada22b
80LK <80LK@users.noreply.github.com>
2018-11-02 19:19:24 +0300

    Add translate
    Add translate

--- main.js
+++ main.js
@@ -1,3 +1,138 @@
+/*
+BUILD INFO:
+  dir: dev
+  target: main.js
+  files: 2
+*/
+
+
+
+// file: translate.js
+
+//general
+Translation.addTranslation("Set both positions.",{
+	ru:"Установите обе позиции.",
+	en:"Set both positions.",
+});
+Translation.addTranslation("Don't valid command.",{
+	ru:"Недействительная команда..",
+	en:"Don't valid command.",
+});
+Translation.addTranslation("%count% block changed.",{
+	ru:"%count% блок изменено.",
+	en:"%count% block changed.",
+});
+Translation.addTranslation("%count% blocks changed.",{
+	ru:"%count% блоков изменено.",
+	en:"%count% blocks changed.",
+});
+Translation.addTranslation("%count% block.",{
+	ru:"%count% блок.",
+	en:"%count% block.",
+});
+Translation.addTranslation("%count% blocks.",{
+	ru:"%count% блоков.",
+	en:"%count% blocks.",
+});
+Translation.addTranslation("Block ID %id%:%data%.",{
+	ru:"ID блока %id%:%data%.",
+	en:"Block ID %id%:%data%.",
+});
+//set
+Translation.addTranslation("Set all blocks inside the selection region to a specified block.", {
+	ru:"Установите все блоки внутри выбранной области в указанный блок.",
+	en:"Set all blocks inside the selection region to a specified block.",
+});
+//box
+Translation.addTranslation("Build walls, floor, and ceiling.", {
+	ru:"Построить стены, пол и потолок",
+	en:"Build walls, floor, and ceiling.",
+});
+//set wool
+Translation.addTranslation("Build the walls of the region (not including ceiling and floor).", {
+	ru:"Построить стены региона (не включая потолок и пол).",
+	en:"Build the walls of the region (not including ceiling and floor).",
+});
+//replace
+Translation.addTranslation("Replace all blocks of the specified block(s) with another block inside the region.", {
+	ru:"Замените все выбранные блоки другим блоком внутри региона.",
+	en:"Replace all blocks of the specified block(s) with another block inside the region.",
+});
+//help
+Translation.addTranslation("Help.", {
+	ru:"Помощь.",
+	en:"Help.",
+});
+Translation.addTranslation("===Help [Page %page%]===\n%cmd%===Help [Page %page%]===", {
+	ru:"===Помощь(Страница %page%)===\n%cmd%===Помощь(Страница %page%)===",
+	en:"===Help [Page %page%]===\n%cmd%===Help [Page %page%]===",
+});
+//pos
+Translation.addTranslation("Set selection position #1 to the block above the one that you are standing on.", {
+	ru:"Установить позицию 1 на блок выше того, на котором вы стоите.",
+	en:"Set selection position #1 to the block above the one that you are standing on.",
+});
+Translation.addTranslation("Set selection position #2 to the block above the one that you are standing on.", {
+	ru:"Установить позицию 2 на блок выше того, на котором вы стоите.",
+	en:"Set selection position #2 to the block above the one that you are standing on.",
+});
+Translation.addTranslation("The first position is set to %x%,%y%,%z%.",{
+	ru:"Первая позиция установлена в %x%,%y%,%z%.",
+	en:"The first position is set to %x%,%y%,%z%.",
+});
+Translation.addTranslation("The second position is set to %x%,%y%,%z%.",{
+	ru:"Вторая позиция установлена в %x%,%y%,%z%.",
+	en:"The second position is set to %x%,%y%,%z%.",
+});
+Translation.addTranslation("The selected region is %sizeArea%",{
+	ru:"Выбранный регион составляет %sizeArea%",
+	en:"The selected region is %sizeArea%",
+});
+//undo
+Translation.addTranslation("Undo your last action.", {
+	ru:"Отменить последние действие.",
+	en:"Undo your last action.",
+});
+Translation.addTranslation("Redo your last (undone) action. This command replays back history and does not repeat the command.", {
+	ru:"Повторите последнее(отмененное) действие.",
+	en:"Redo your last (undone) action. This command replays back history and does not repeat the command.",
+});
+//wand
+Translation.addTranslation("Gives you the \"EditWand\" (by default, a wooden axe).", {
+	ru:"Дает вам \"EditWand\" (по умолчанию - деревянный топор).",
+	en:"Gives you the \"EditWand\" (by default, a wooden axe).",
+});
+//region
+Translation.addTranslation("Work with the region.", {
+	ru:"Работа с регионом.",
+	en:"Work with the region.",
+});
+Translation.addTranslation("Commands for working with the region.", {
+	ru:"Команды для работы с регионом.",
+	en:"Commands for working with the region.",
+});
+Translation.addTranslation("Raise the selected region by the specified number of blocks.", {
+	ru:"Поднять выделенный регион на указанное количество блоков.",
+	en:"Raise the selected region by the specified number of blocks.",
+});
+Translation.addTranslation("Lower the selected region by the specified number of blocks.", {
+	ru:"Опустить выделенный регион на указанное количество блоков.",
+	en:"Lower the selected region by the specified number of blocks.",
+});
+Translation.addTranslation("The region is raised to %area%", {
+	ru:"Регион поднят на %area%",
+	en:"The region is raised to %area%",
+});
+Translation.addTranslation("The region is omitted in %area%", {
+	ru:"Регион опущен на %area%",
+	en:"The region is omitted in %area%",
+});
+
+
+
+
+// file: main.js
+
 var WorldEdit = {
 	pos1:{x:Infinity,y:Infinity,z:Infinity},
 	pos2:{x:Infinity,y:Infinity,z:Infinity},
@@ -421,7 +556,7 @@ var Commands = {
 				
 			}
 			Game.message(Translation.translate("The first position is set to %x%,%y%,%z%.").replace("%x%",coords.x).replace("%y%",coords.y).replace("%z%",coords.z));
-			Game.message(Translation.translate("The selected region is %sizeArea%").replace("%sizeArea%", sizeArea));
+			Game.message("The selected region is %sizeArea%".replace("%sizeArea%", sizeArea));
 		}else{
 			if(!args[1] || !args[2]){
 				return Game.message(Translation.translate("Don't valid command."));
@@ -442,7 +577,7 @@ var Commands = {
 					
 				}
 				Game.message(Translation.translate("The first position is set to %x%,%y%,%z%.").replace("%x%",args[0]).replace("%y%",args[1]).replace("%z%",args[2]));
-				Game.message(Translation.translate("The selected region is %sizeArea%").replace("%sizeArea%", sizeArea));
+				Game.message("The selected region is %sizeArea%".replace("%sizeArea%", sizeArea));
 			}
 		}
 	}
@@ -470,7 +605,7 @@ var Commands = {
 				
 			}
 			Game.message(Translation.translate("The second position is set to %x%,%y%,%z%.").replace("%x%",coords.x).replace("%y%",coords.y).replace("%z%",coords.z));
-			Game.message(Translation.translate("The selected region is %sizeArea%").replace("%sizeArea%", sizeArea));
+			Game.message("The selected region is %sizeArea%".replace("%sizeArea%", sizeArea));
 		}else{
 			if(!args[1] || !args[2]){
 				return Game.message(Translation.translate("Don't valid command."));
@@ -491,7 +626,7 @@ var Commands = {
 					
 				}
 				Game.message(Translation.translate("The second position is set to %x%,%y%,%z%.").replace("%x%",args[0]).replace("%y%",args[1]).replace("%z%",args[2]));
-				Game.message(Translation.translate("The selected region is %sizeArea%").replace("%sizeArea%", sizeArea));
+				Game.message("The selected region is %sizeArea%".replace("%sizeArea%", sizeArea));
 			}
 		}
 	}
@@ -582,3 +717,7 @@ Callback.addCallback("DestroyBlockStart", function (coords, block, player) {
 		Game.prevent();
 	}
 });
+
+
+
+
